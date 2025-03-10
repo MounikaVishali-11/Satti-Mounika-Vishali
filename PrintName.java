52f76d8315f5154c904f467b5c1eedfaeae91762
MounikaVishali-11 <mounikavishali54@gmail.com>
2025-03-10 11:31:34 +0530

    Add files via upload

---
+++ PrintName.java
@@ -0,0 +1,8 @@
+public class PrintName {
+public static void main(String[] args) {
+    String name = "Mounika";
+    /* The System.out.print() is a very frequently used method
+        for printing to the console or the standard output. */
+    System.out.println("My name is " + name);
+}
+}
