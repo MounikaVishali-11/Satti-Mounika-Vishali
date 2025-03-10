21ba992d4f6780f5489bee56abc1b6b208c51fce
MounikaVishali-11 <mounikavishali54@gmail.com>
2025-03-10 11:33:55 +0530

    Add files via upload

---
+++ AccessingProtectedFieldsMethod.java
@@ -0,0 +1,12 @@
+//Accessing the PROTECTED fields and methods from child class
+public class AccessingProtectedFieldsMethod extends ProtectedClassFieldsMethod {
+
+    public static void main(String[] args) {
+        //Creating object of AccessingProtectedFieldsMethod
+        AccessingProtectedFieldsMethod sub = new AccessingProtectedFieldsMethod();
+        //Accessing protected field
+        sub.name = "Protected Method in SubClass";
+        //Accessing protected method
+        sub.protectedMethod();
+    }
+}
---
+++ InterfaceOneMethod.java
@@ -0,0 +1,17 @@
+interface Jala_01 {
+    //An abstract method doesn't have its body, and they are public and abstract
+    void oneMethod();
+}
+
+public class InterfaceOneMethod implements Jala_01 {
+    @Override
+    public void oneMethod() {
+        System.out.println("This is a method");
+    }
+
+    public static void main(String[] args) {
+        InterfaceOneMethod if1 = new InterfaceOneMethod();
+        //Calling the method implemented
+        if1.oneMethod();
+    }
+}
